ed0d285ed33be4f2902b9e2fc26400e2b329e59a
tlp19-pi <tlp19@ic.ac.uk>
2023-06-13 16:24:48 +0100

    set max brightness to 0.1 to minimize current

--- leds_test.py
+++ leds_test.py
@@ -1,6 +1,6 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 60, bpp=3, brightness=0.2)
+pixels = neopixel.NeoPixel(board.D18, 60, bpp=3, brightness=0.1)
 
 pixels.fill((255,255,255))
